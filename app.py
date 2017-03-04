62d24ea91cf5a01318a1f9e8b507f4ae7acc90d5
admin <admin@admins-Mac-Pro-3.local>
2017-03-04 16:18:40 -0500

    Made detail page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import csv 
+from flask import abort
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -19,6 +20,18 @@ def index():
 	object_list = get_csv()
 	#after the comma, goes the thing you want to put onto the template
 	return render_template(template, object_list = object_list)
+
+#Here we are setting up a pattern for the url so that it connects the row id to the url.
+#This is all to make a new url for every person in the dataset. 
+@app.route('/<row_id>/')
+def detail(row_id):
+	template = "detail.html"
+	object_list = get_csv()
+	for row in object_list:
+		if row["id"] == row_id:
+			return render_template(template, object=row)
+	abort(404)
+
 
 # We need a place to store our template
 
